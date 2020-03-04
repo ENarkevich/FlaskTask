d94db802f1cfd2e4bcd7512d0bd730b14a0f7970
Narkevich <ekarerinsss@gmail.com>
2020-03-04 17:29:04 +0300

    new commit

--- client.py
+++ client.py
@@ -6,19 +6,21 @@ import json
 from config import CLIENT_FOLDER, SERV
 
 if __name__ == "__main__":
-    parts_number = 10000
+    parts_number = 15
     request_data = {}
     #request fot cropp
     request_data["parts_number"] = parts_number
-    request_data["path_file"] = f'{CLIENT_FOLDER}pict.jpg'
+    request_data["path_file"] = f'{CLIENT_FOLDER}picttt.jpg'
     responce_parts = requests.post(SERV, json=request_data)  
     if (responce_parts.status_code==200):
         parts = responce_parts.json()
         # request for concatenate 
         for_concatenate=parts
         from_conc = requests.post(f'{SERV}concatenate', json=for_concatenate)
+        # decode result from json 
         from_conc = from_conc.json()
         pict = base64.b64decode(from_conc["result"])
         decoded = cv2.imdecode(np.frombuffer(pict, dtype=np.uint8), flags=1)
         cv2.imwrite(f'{CLIENT_FOLDER}result.jpg', decoded)
-
+    else:
+        print(responce_parts.headers["message"])
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response, make_response, json, abort
+from flask import Flask, request, Response, make_response, json
 import cv2
 import numpy as np
 import base64
@@ -11,42 +11,46 @@ app = Flask(__name__)
 def upload_file():
     f = request.json
     if (not(os.path.exists(f["path_file"]))):
-        print("File not found")
-        return Response(status=404)
+        return Response(status=404, headers={"message":"File not found"}) 
     image = cv2.imread(f["path_file"])
     parts_number = f["parts_number"]
     responce_data={}
     parts, responce_data["ratio_y"], responce_data["ratio_x"] = crop_file(image, parts_number)
+    # if number of parts is unreal
     if (parts==None):
-        return abort(409, 'My custom message')
-        #return Response(status=409)     # if number of pars is unreal 
+        return Response(status=409, headers={"message":"Impossible to cropp to this number of parts"})
+    # encode data to json 
     for i in range(parts_number):
         responce_data[str(i)] = base64.b64encode(cv2.imencode('.jpg', parts[i])[1]).decode() 
     return make_response(json.dumps(responce_data)) 
     
 def crop_file(image, number_parts):
-    height, width = image.shape[0], image.shape[1]
-    #height, width = image.shape
+    height, width, _ = image.shape
     parts = []
     ratio = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
-    ratio_x = [i for i in ratio if number_parts%i==0]    # search for X-proportion 
+    # search for X-proportion
+    ratio_x = [i for i in ratio if number_parts%i==0]     
     ratio_x = ratio_x[-1]
-    ratio_y = int(number_parts/ratio_x)     # Y-proportion for cropp
+    # Y-proportion for cropp
+    ratio_y = int(number_parts/ratio_x)
     if (height>width and ratio_x>ratio_y):  
         ratio_x, ratio_y = ratio_y, ratio_x
-    if(ratio_x>width or ratio_y>height):    # if number of pars is unreal 
+    # if number of parts is unreal
+    if(ratio_x>width or ratio_y>height):
         return None, None, None
-    x_offset, y_offset = int(width/ratio_x), int(height/ratio_y)    # shift for new points
-    x1, y1 = 0, 0      # top poin of frame
-    x2, y2 = x_offset, y_offset     # bottom point of frame 
+    # shift for new points
+    x_offset, y_offset = int(width/ratio_x), int(height/ratio_y)  
+    x1, y1 = 0, 0     
+    x2, y2 = x_offset, y_offset   
+    # cropp picture
     for i in range(ratio_y):
         for j in range(ratio_x): 
             parts.append(image[y1:y2,x1:x2])  
-            x1 = x1 + x_offset  # X-cordinate for next frame
+            x1 = x1 + x_offset
             x2 = x2 + x_offset
             if (j==ratio_x-1):
                 x2 =  width
-        y1 = y1 + y_offset  # Y-cordinate for next frame
+        y1 = y1 + y_offset
         y2 = y2 + y_offset
         if(i==ratio_y-1):
             y2 = height
@@ -59,21 +63,24 @@ def concatenate_parts():
     ratio_x=f["ratio_x"]
     ratio_y=f["ratio_y"]
     parts=[]
+    # decode from json 
     for i in range(ratio_x*ratio_y):
         pict = base64.b64decode(f[str(i)])
         parts.append(cv2.imdecode(np.frombuffer(pict, dtype=np.uint8), flags=1))
     all_picture=np.array([])
+    # concatenate horizontal lines to picture
     for i in range(ratio_y):
         frame = parts.pop()
+        # concatenate parts to horizontal lines
         for j in range(ratio_x-1):
             frame = np.concatenate((parts.pop(), frame), axis=1)
         if (i>0):
             all_picture = np.concatenate((frame, all_picture), axis=0)
         else:
             all_picture = frame
-
     all_picture = cv2.bitwise_not(all_picture)
     responce_data={}
+    # encode data to json
     responce_data["result"] = base64.b64encode(cv2.imencode('.jpg', all_picture)[1]).decode() 
     return make_response(json.dumps(responce_data))
 
